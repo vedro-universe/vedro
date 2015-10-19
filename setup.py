9594a48597bb6ae581e20a9ea721459ad5d6dd51
Nikita Tsvetkov <nikitanovosibirsk@yandex.com>
2015-10-19 19:20:33 +0600

    initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+
+setup(
+  name='vedro',
+  description='',
+  version='0.1.0',
+  url='https://github.com/nikitanovosibirsk/vedro',
+  author='Nikita Tsvetkov',
+  author_email='nikitanovosibirsk@yandex.com',
+  license='MIT',
+  packages=find_packages()
+)
