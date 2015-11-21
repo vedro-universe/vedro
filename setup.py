821763212c617ef71cb739246a7a3f75e3f6c315
Nikita Tsvetkov <nikitanovosibirsk@yandex.com>
2015-11-21 21:09:26 +0600

    add package_data

--- setup.py
+++ setup.py
@@ -4,12 +4,13 @@ from setuptools import setup, find_packages
 setup(
   name='vedro',
   description='',
-  version='0.2.0',
+  version='0.2.1',
   url='https://github.com/nikitanovosibirsk/vedro',
   author='Nikita Tsvetkov',
   author_email='nikitanovosibirsk@yandex.com',
   license='MIT',
   packages=find_packages(),
+  package_data={'vedro': ['vedro.cfg']},
   install_requires=[
     'colorama>=0.3.3',
     'district42>=0.5.4',
