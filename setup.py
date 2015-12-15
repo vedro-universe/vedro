d72295cf4b555ca47b8d851f4808d65d1c6c166b
Nikita Tsvetkov <nikitanovosibirsk@yandex.com>
2015-12-15 18:43:38 +0600

    update setup.py

--- setup.py
+++ setup.py
@@ -11,10 +11,12 @@ setup(
   license='MIT',
   packages=find_packages(),
   package_data={'vedro': ['vedro.cfg']},
+  dependency_links=[
+    'https://github.com/nikitanovosibirsk/blahblah/tarball/2b49c2e8403d6f83dfbbb5579caccc46915a561b#egg=blahblah',
+    'https://github.com/nikitanovosibirsk/valeera/tarball/74a119b57ce13bc573aa4f6a8a585379183052f4#egg=valeera',
+    'https://github.com/nikitanovosibirsk/district42/tarball/dc9db4f5a261dd095642fdd8a564c067600b2309#egg=district42'
+  ],
   install_requires=[
-    'colorama>=0.3.3',
-    'district42>=0.5.5',
-    'valeera>=0.5.5',
-    'blahblah>=0.5.7'
+    'colorama>=0.3.3'
   ]
 )
