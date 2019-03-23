02441545bbd205d5536b425e075d1fa3d62a6ef9
Nikita Tsvetkov <nikitanovosibirsk@yandex.com>
2019-03-23 18:41:50 +0700

    add scenario tags

--- vedro/core/runner.py
+++ vedro/core/runner.py
@@ -38,7 +38,7 @@ class Runner:
     except:
       subject = scope['subject']
     steps = [Step(scope[x.co_name]) for x in fn.__code__.co_consts if inspect.iscode(x)]
-    tags = getattr(fn, '__tags__', [])
+    tags = set(getattr(fn, '__tags__', [])) | set(scope.get('tags', []))
     return Scenario(path, namespace, fn, scope, subject, steps, tags)
 
   def __load_scenarios(self, path, namespace):
