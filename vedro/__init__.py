1b1bc5f798828ec41529fe390ab74584a6a55b3e
Nikita Tsvetkov <nikitanovosibirsk@yandex.com>
2020-07-20 21:02:53 +0700

    update runner

--- vedro/__init__.py
+++ vedro/__init__.py
@@ -14,14 +14,4 @@ __all__ = ("Scenario", "Runner", "run", "only", "skip", "params",)
 
 def run(*, validator: Optional[Validator] = None) -> None:
     runner = Runner(validator=validator)
-    loop = asyncio.get_event_loop()
-    try:
-        loop.run_until_complete(runner.run())
-    except Exception as e:
-        print("Runtime Exception", e)
-        raise
-    finally:
-        try:
-            loop.run_until_complete(loop.shutdown_asyncgens())
-        finally:
-            loop.close()
+    asyncio.run(runner.run())
