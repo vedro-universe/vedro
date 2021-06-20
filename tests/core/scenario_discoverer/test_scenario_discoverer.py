8bdb9e867760ba3ddd32c82a987f5dcc181fdbcf
Nikita Tsvetkov <nikitanovosibirsk@yandex.com>
2021-06-20 12:28:21 +0700

    fix discoverer test

--- tests/core/scenario_discoverer/test_scenario_discoverer.py
+++ tests/core/scenario_discoverer/test_scenario_discoverer.py
@@ -34,8 +34,8 @@ def create_scenario(filename):
 async def test_scenario_discoverer(*, finder_factory, loader_factory):
     root = Path("/tmp")
     scenario1 = create_scenario(root / "scenario-1.py")
-    scenario3 = create_scenario(root / "folder" / "scenario-3.py")
-    scenario4 = create_scenario(root / "folder" / "scenario-4.py")
+    scenario3 = create_scenario(root / "folder" / "scenario-2.py")
+    scenario4 = create_scenario(root / "folder" / "scenario-10.py")
     tree = {
         scenario1.__file__: [scenario1],
         root / "scenario-2.py": [],
