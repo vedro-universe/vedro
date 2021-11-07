d28dbaa03156154fe3ceadd5d5776d551ef807fb
Nikita Tsvetkov <nikitanovosibirsk@yandex.com>
2021-11-07 12:33:44 +0300

    fix decorators

--- vedro/plugins/skipper/_skip.py
+++ vedro/plugins/skipper/_skip.py
@@ -1,18 +1,19 @@
-from typing import Any, Type, Union
+from inspect import isclass
+from typing import Any, Optional, Type, Union
 
 from vedro import Scenario
 
 __all__ = ("skip",)
 
 
-def skip(scenario_or_reason: Union[Type[Scenario], str]) -> Any:
+def skip(scenario_or_reason: Optional[Union[Type[Scenario], str]] = None) -> Any:
     def wrapped(scenario: Type[Scenario]) -> Type[Scenario]:
         setattr(scenario, "__vedro__skipped__", True)
         return scenario
 
-    if isinstance(scenario_or_reason, str):
+    if (scenario_or_reason is None) or isinstance(scenario_or_reason, str):
         return wrapped
-    elif issubclass(scenario_or_reason, Scenario):
+    elif isclass(scenario_or_reason) and issubclass(scenario_or_reason, Scenario):
         return wrapped(scenario_or_reason)
     else:
         raise TypeError()
