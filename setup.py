3aa4cb7b18b0453de296c133d1c78ca98af5a8cd
Nikita Tsvetkov <nikitanovosibirsk@yandex.com>
2021-12-16 21:26:01 +0300

    bump version → 1.3.4

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def find_dev_required():
 
 setup(
     name="vedro",
-    version="1.3.3",
+    version="1.3.4",
     description="",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
