2014144e59ae5fdac5e7adf320fca4f1a0be39d0
Nikita Tsvetkov <nikitanovosibirsk@yandex.com>
2022-05-03 12:31:10 +0300

    update description

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def find_dev_required():
 setup(
     name="vedro",
     version="1.5.0",
-    description="BDD Framework (without magic)",
+    description="Pragmatic BDD Framework",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     author="Nikita Tsvetkov",
