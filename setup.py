316e340ab845b8e892b47be2fc22fdb15ec14a64
Nikita Tsvetkov <nikitanovosibirsk@yandex.com>
2022-06-05 15:19:54 +0300

    bump version → 1.6.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def find_dev_required():
 
 setup(
     name="vedro",
-    version="1.5.1",
+    version="1.6.0",
     description="Pragmatic BDD Framework",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
