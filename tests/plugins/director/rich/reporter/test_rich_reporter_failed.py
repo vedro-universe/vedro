f53388b85508e3c0a609de14f57a29e71b4f8662
Nikita Tsvetkov <nikitanovosibirsk@yandex.com>
2022-09-03 18:43:57 +0300

    add tests

--- tests/plugins/director/rich/reporter/test_rich_reporter_failed.py
+++ tests/plugins/director/rich/reporter/test_rich_reporter_failed.py
@@ -147,3 +147,65 @@ async def test_scenario_failed_verbose2_show_timings(dispatcher: Dispatcher,
 
             call.print_exception(exc_info, max_frames=8, show_internal_calls=False)
         ]
+
+
+@pytest.mark.asyncio
+@pytest.mark.usefixtures(rich_reporter.__name__)
+async def test_scenario_failed_verbose3_without_scope(*, dispatcher: Dispatcher, printer_: Mock):
+    with given:
+        await fire_arg_parsed_event(dispatcher, verbose=3)
+
+        scenario_result = make_scenario_result()
+        scenario_result.set_scope({})
+
+        aggregated_result = make_aggregated_result(scenario_result.mark_failed())
+        event = ScenarioReportedEvent(aggregated_result)
+
+    with when:
+        await dispatcher.fire(event)
+
+    with then:
+        assert printer_.mock_calls == [
+            call.print_scenario_subject(aggregated_result.scenario.subject,
+                                        ScenarioStatus.FAILED, elapsed=None, prefix=" "),
+        ]
+
+
+@pytest.mark.asyncio
+@pytest.mark.usefixtures(rich_reporter.__name__)
+async def test_scenario_failed_verbose3_with_scope(*, dispatcher: Dispatcher, printer_: Mock):
+    with given:
+        await fire_arg_parsed_event(dispatcher, verbose=3)
+
+        scenario_result = make_scenario_result()
+
+        step_result_passed = make_step_result().mark_passed()
+        scenario_result.add_step_result(step_result_passed)
+
+        exc_info = make_exc_info(AssertionError())
+        step_result_failed = make_step_result().mark_failed().set_exc_info(exc_info)
+        scenario_result.add_step_result(step_result_failed)
+
+        scope = {"key": "val"}
+        scenario_result.set_scope(scope)
+
+        aggregated_result = make_aggregated_result(scenario_result.mark_failed())
+        event = ScenarioReportedEvent(aggregated_result)
+
+    with when:
+        await dispatcher.fire(event)
+
+    with then:
+        assert printer_.mock_calls == [
+            call.print_scenario_subject(aggregated_result.scenario.subject,
+                                        ScenarioStatus.FAILED, elapsed=None, prefix=" "),
+
+            call.print_step_name(step_result_passed.step_name,
+                                 StepStatus.PASSED, elapsed=None, prefix=" " * 3),
+            call.print_step_name(step_result_failed.step_name,
+                                 StepStatus.FAILED, elapsed=None, prefix=" " * 3),
+
+            call.print_pretty_exception(exc_info, max_frames=8, show_locals=False,
+                                        show_internal_calls=False),
+            call.print_scope(scope)
+        ]
