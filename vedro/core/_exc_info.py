0b0de9c6be31c7741f6428ac17929b2d1d12d67e
Nikita Tsvetkov <nikitanovosibirsk@yandex.com>
2022-09-04 12:02:33 +0300

    rever exc_info changes

--- vedro/core/_exc_info.py
+++ vedro/core/_exc_info.py
@@ -9,21 +9,9 @@ class ExcInfo:
                  type_: Type[BaseException],
                  value: BaseException,
                  traceback: TracebackType) -> None:
-        self._type = type_
-        self._value = value
-        self._traceback = traceback
-
-    @property
-    def type(self) -> Type[BaseException]:
-        return self._type
-
-    @property
-    def value(self) -> BaseException:
-        return self._value
-
-    @property
-    def traceback(self) -> TracebackType:
-        return self._traceback
+        self.type = type_
+        self.value = value
+        self.traceback = traceback
 
     def __repr__(self) -> str:
-        return f"{self.__class__.__name__}({self._type!r}, {self._value!r}, {self._traceback!r})"
+        return f"{self.__class__.__name__}({self.type!r}, {self.value!r}, {self.traceback!r})"
