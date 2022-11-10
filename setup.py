418cd6e5c47929389f8eb9b4cda8eec5d06164a9
Nikita Tsvetkov <nikitanovosibirsk@yandex.com>
2022-11-10 22:35:32 +0400

    add python3.11

--- setup.py
+++ setup.py
@@ -34,12 +34,12 @@ setup(
     install_requires=find_required(),
     tests_require=find_dev_required(),
     classifiers=[
-        "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.10",
         "Topic :: Software Development",
         "Typing :: Typed",
