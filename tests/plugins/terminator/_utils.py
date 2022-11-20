95d304461b6a2f5078de8e532719d231de8d3a52
Nikita Tsvetkov <nikitanovosibirsk@yandex.com>
2022-11-20 13:01:12 +0400

    add terminator tests

--- tests/plugins/terminator/_utils.py
+++ tests/plugins/terminator/_utils.py
@@ -1,10 +1,13 @@
+import sys
 from pathlib import Path
 from time import monotonic_ns
+from types import TracebackType
+from typing import Optional, cast
 
 import pytest
 
 from vedro import Scenario
-from vedro.core import Dispatcher, ScenarioResult, VirtualScenario
+from vedro.core import AggregatedResult, Dispatcher, ExcInfo, ScenarioResult, VirtualScenario
 from vedro.plugins.terminator import Terminator, TerminatorPlugin
 
 
@@ -29,3 +32,17 @@ def make_vscenario() -> VirtualScenario:
 
 def make_scenario_result() -> ScenarioResult:
     return ScenarioResult(make_vscenario())
+
+
+def make_aggregated_result(scenario_result: Optional[ScenarioResult] = None) -> AggregatedResult:
+    if scenario_result is None:
+        scenario_result = make_scenario_result()
+    return AggregatedResult.from_existing(scenario_result, [scenario_result])
+
+
+def make_exc_info(exc_val: BaseException) -> ExcInfo:
+    try:
+        raise exc_val
+    except type(exc_val):
+        *_, traceback = sys.exc_info()
+    return ExcInfo(type(exc_val), exc_val, cast(TracebackType, traceback))
