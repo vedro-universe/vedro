040ac8c92f5353a9824541119f86ba09b41adffc
Nikita Tsvetkov <nikitanovosibirsk@yandex.com>
2022-11-20 19:44:46 +0400

    add signal handler

--- tests/plugins/interrupter/test_interrupter_plugin.py
+++ tests/plugins/interrupter/test_interrupter_plugin.py
@@ -1,24 +1,30 @@
+import signal
 from typing import Callable
+from unittest.mock import Mock
 
 import pytest
 from _pytest.python_api import raises
 from baby_steps import given, then, when
 
 from vedro.core import Dispatcher, Event
+from vedro.core import MonotonicScenarioScheduler as Scheduler
+from vedro.core import Report
 from vedro.core.scenario_runner import Interrupted
-from vedro.events import ScenarioRunEvent, ScenarioSkippedEvent
+from vedro.events import CleanupEvent, ScenarioRunEvent, ScenarioSkippedEvent, StartupEvent
 from vedro.plugins.interrupter import InterrupterPluginTriggered
 
 from ._utils import (
+    HANDLE_SIGNAL,
     dispatcher,
     fire_arg_parsed_event,
     fire_scenario_reported_event,
     interrupter,
     make_aggregated_result,
     make_scenario_result,
+    sig_handler_,
 )
 
-__all__ = ("dispatcher", "interrupter",)  # fixtures
+__all__ = ("dispatcher", "interrupter", "sig_handler_",)  # fixtures
 
 
 @pytest.mark.asyncio
@@ -117,3 +123,72 @@ async def test_scenario_run_skipped_fail_fast(get_event: Callable[[], Event], *,
 
     with then:
         assert "no exception"
+
+
+@pytest.mark.asyncio
+@pytest.mark.usefixtures(interrupter.__name__)
+async def test_signal_handler(sig_handler_: Mock, dispatcher: Dispatcher):
+    with given:
+        await fire_arg_parsed_event(dispatcher, fail_fast=True)
+
+        scheduler = Scheduler([])
+        await dispatcher.fire(StartupEvent(scheduler))
+
+    with when, raises(BaseException) as exc:
+        signal.raise_signal(HANDLE_SIGNAL)
+
+    with then:
+        assert exc.type is InterrupterPluginTriggered
+        assert isinstance(exc.value, Interrupted)
+        assert str(exc.value) == f"Stop after signal {HANDLE_SIGNAL!r} received"
+
+        assert len(sig_handler_.mock_calls) == 0
+
+
+@pytest.mark.asyncio
+@pytest.mark.usefixtures(interrupter.__name__)
+async def test_signal_handler_no_fail_fast(sig_handler_: Mock, dispatcher: Dispatcher):
+    with given:
+        await fire_arg_parsed_event(dispatcher, fail_fast=False)
+
+        scheduler = Scheduler([])
+        await dispatcher.fire(StartupEvent(scheduler))
+
+    with when:
+        signal.raise_signal(HANDLE_SIGNAL)
+
+    with then:
+        assert len(sig_handler_.mock_calls) == 1
+
+
+@pytest.mark.asyncio
+@pytest.mark.usefixtures(interrupter.__name__)
+async def test_signal_handler_cleanup(sig_handler_: Mock, dispatcher: Dispatcher):
+    with given:
+        await fire_arg_parsed_event(dispatcher, fail_fast=True)
+
+        scheduler = Scheduler([])
+        await dispatcher.fire(StartupEvent(scheduler))
+
+        event = CleanupEvent(Report())
+
+    with when:
+        await dispatcher.fire(event)
+
+    with then:
+        assert signal.getsignal(HANDLE_SIGNAL) == sig_handler_
+
+
+@pytest.mark.asyncio
+@pytest.mark.usefixtures(interrupter.__name__)
+async def test_cleanup(sig_handler_: Mock, dispatcher: Dispatcher):
+    with given:
+        await fire_arg_parsed_event(dispatcher, fail_fast=False)
+
+        event = CleanupEvent(Report())
+
+    with when:
+        await dispatcher.fire(event)
+
+    with then:
+        assert signal.getsignal(HANDLE_SIGNAL) == sig_handler_
--- vedro/plugins/interrupter/_interrupter.py
+++ vedro/plugins/interrupter/_interrupter.py
@@ -1,13 +1,17 @@
-from typing import Type, Union
+import signal
+from types import FrameType
+from typing import Any, Dict, Optional, Tuple, Type, Union
 
 from vedro.core import Dispatcher, Plugin, PluginConfig
 from vedro.core.scenario_runner import Interrupted
 from vedro.events import (
     ArgParsedEvent,
     ArgParseEvent,
+    CleanupEvent,
     ScenarioReportedEvent,
     ScenarioRunEvent,
     ScenarioSkippedEvent,
+    StartupEvent,
 )
 
 __all__ = ("Interrupter", "InterrupterPlugin", "InterrupterPluginTriggered",)
@@ -22,13 +26,17 @@ class InterrupterPlugin(Plugin):
         super().__init__(config)
         self._fail_fast: bool = False
         self._failed: int = 0
+        self._signals: Tuple[signal.Signals, ...] = config.handle_signals
+        self._orig_handlers: Dict[signal.Signals, Any] = {}
 
     def subscribe(self, dispatcher: Dispatcher) -> None:
         dispatcher.listen(ArgParseEvent, self.on_arg_parse) \
                   .listen(ArgParsedEvent, self.on_arg_parsed) \
+                  .listen(StartupEvent, self.on_startup) \
                   .listen(ScenarioRunEvent, self.on_scenario_run) \
                   .listen(ScenarioSkippedEvent, self.on_scenario_run) \
-                  .listen(ScenarioReportedEvent, self.on_scenario_reported)
+                  .listen(ScenarioReportedEvent, self.on_scenario_reported) \
+                  .listen(CleanupEvent, self.on_cleanup)
 
     def on_arg_parse(self, event: ArgParseEvent) -> None:
         event.arg_parser.add_argument("--fail-fast", "--ff", "-f",
@@ -39,6 +47,21 @@ class InterrupterPlugin(Plugin):
     def on_arg_parsed(self, event: ArgParsedEvent) -> None:
         self._fail_fast = event.args.fail_fast
 
+    def on_startup(self, event: StartupEvent) -> None:
+        if not self._fail_fast:
+            return
+
+        def handler(signum: int, frame: Optional[FrameType]) -> None:
+            try:
+                signame = signal.Signals(signum)
+            except ValueError:
+                signame = signum  # type: ignore
+            raise InterrupterPluginTriggered(f"Stop after signal {signame!r} received")
+
+        for sig in self._signals:
+            self._orig_handlers[sig] = signal.getsignal(sig)
+            signal.signal(sig, handler)
+
     def on_scenario_reported(self, event: ScenarioReportedEvent) -> None:
         if event.aggregated_result.is_failed():
             self._failed += 1
@@ -47,6 +70,17 @@ class InterrupterPlugin(Plugin):
         if self._fail_fast and self._failed > 0:
             raise InterrupterPluginTriggered("Stop after first failed scenario")
 
+    def on_cleanup(self, event: CleanupEvent) -> None:
+        if not self._fail_fast:
+            return
+
+        for sig, orig_handler in self._orig_handlers.items():
+            signal.signal(sig, orig_handler)
+        self._orig_handlers = {}
+
 
 class Interrupter(PluginConfig):
     plugin = InterrupterPlugin
+
+    # Raise Interrupted exception on these signals
+    handle_signals: Tuple[signal.Signals, ...] = (signal.SIGTERM,)
