c29f2f646353bf29f98c8483020505aefeca7de1
Nikita Tsvetkov <tsv1@fastmail.com>
2023-05-03 00:54:25 +0400

    fix warn message

--- vedro/events/__init__.py
+++ vedro/events/__init__.py
@@ -64,7 +64,7 @@ class StartupEvent(Event):
 
     @property
     def scenarios(self) -> List[VirtualScenario]:
-        warnings.warn("Deprecated: use scheduler.scenarios instead", DeprecationWarning)
+        warnings.warn("Deprecated: use scheduler.scheduled instead", DeprecationWarning)
         return list(self._scheduler.discovered)
 
     def __repr__(self) -> str:
