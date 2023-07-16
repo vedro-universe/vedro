b0a0b8e51de898faeca808b71aa1a5890bfe77a0
Nikita Tsvetkov <tsv1@fastmail.com>
2023-07-16 12:55:58 +0400

    add test

--- tests/plugins/interrupter/test_interrupter_plugin.py
+++ tests/plugins/interrupter/test_interrupter_plugin.py
@@ -32,8 +32,18 @@ __all__ = ("dispatcher", "interrupter", "sig_handler_",)  # fixtures
 
 @pytest.mark.asyncio
 @pytest.mark.usefixtures(interrupter.__name__)
+async def test_failed_count_validation(*, dispatcher: Dispatcher):
+    with when, raises(BaseException) as exc:
+        await fire_arg_parsed_event(dispatcher, fail_fast=None, fail_after_count=0)
+
+    with then:
+        assert isinstance(exc.value, ValueError)
+        assert str(exc.value) == "InterrupterPlugin: `fail_after_count` must be >= 1"
+
+
+@pytest.mark.asyncio
+@pytest.mark.usefixtures(interrupter.__name__)
 @pytest.mark.parametrize("get_event", [
-    lambda: ScenarioRunEvent(make_scenario_result()),
     lambda: ScenarioRunEvent(make_scenario_result()),
     lambda: ScenarioSkippedEvent(make_scenario_result()),
 ])
@@ -51,6 +61,30 @@ async def test_no_fail_fast(get_event: Callable[[], Event], *, dispatcher: Dispa
 
     with then:
         assert "no exception"
+
+
+@pytest.mark.asyncio
+@pytest.mark.usefixtures(interrupter.__name__)
+@pytest.mark.parametrize("get_event", [
+    lambda: ScenarioRunEvent(make_scenario_result()),
+    lambda: ScenarioSkippedEvent(make_scenario_result()),
+])
+async def test_fail_fast(get_event: Callable[[], Event], *, dispatcher: Dispatcher):
+    with given:
+        await fire_arg_parsed_event(dispatcher, fail_fast=True, fail_after_count=None)
+
+        scenario_result = make_scenario_result().mark_failed()
+        await fire_scenario_reported_event(dispatcher, make_aggregated_result(scenario_result))
+
+        event = get_event()
+
+    with when, raises(BaseException) as exc:
+        await dispatcher.fire(event)
+
+    with then:
+        assert exc.type is InterrupterPluginTriggered
+        assert isinstance(exc.value, Interrupted)
+        assert str(exc.value) == "Stop after first failed scenario"
 
 
 @pytest.mark.asyncio
