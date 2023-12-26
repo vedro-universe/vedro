8eb4ba6d91e1b8665dcdef97a8d80864eba0cb4a
Nikitа Tsvetkоv <tsv1@fastmail.com>
2023-12-26 17:59:11 +0400

    add module validation (#74)

--- vedro/core/_scenario_discoverer/_create_vscenario.py
+++ vedro/core/_scenario_discoverer/_create_vscenario.py
@@ -9,6 +9,16 @@ __all__ = ("create_vscenario",)
 
 
 def create_vscenario(scenario: Type[Scenario]) -> VirtualScenario:
+    """
+    Creates a VirtualScenario from a given Scenario class.
+
+    This function inspects the given Scenario class, extracts its methods that represent
+    steps, and constructs a VirtualScenario object that encapsulates the scenario and its steps.
+    It skips private methods (those starting with '_') and only includes functions.
+
+    :param scenario: The Scenario class to be converted into a VirtualScenario.
+    :return: A VirtualScenario object containing the scenario and its steps.
+    """
     steps = []
     for step in scenario.__dict__:
         if step.startswith("_"):
