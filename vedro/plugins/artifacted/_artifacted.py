691445b997b1dc1e8c1a9e381393271d589df45c
Nikita Tsvetkov <tsv1@fastmail.com>
2024-01-07 12:51:28 +0400

    update plugin

--- vedro/plugins/artifacted/_artifacted.py
+++ vedro/plugins/artifacted/_artifacted.py
@@ -5,6 +5,7 @@ from typing import Deque, Type, Union
 
 from vedro.core import (
     Artifact,
+    ConfigType,
     Dispatcher,
     FileArtifact,
     MemoryArtifact,
@@ -15,6 +16,7 @@ from vedro.core import (
 from vedro.events import (
     ArgParsedEvent,
     ArgParseEvent,
+    ConfigLoadedEvent,
     ScenarioFailedEvent,
     ScenarioPassedEvent,
     ScenarioReportedEvent,
@@ -51,10 +53,11 @@ class ArtifactedPlugin(Plugin):
         self._scenario_artifacts = scenario_artifacts
         self._step_artifacts = step_artifacts
         self._save_artifacts = config.save_artifacts
-        self._artifacts_dir = ".vedro/artifacts"
+        self._global_config: Union[ConfigType, None] = None
 
     def subscribe(self, dispatcher: Dispatcher) -> None:
-        dispatcher.listen(ArgParseEvent, self.on_arg_parse) \
+        dispatcher.listen(ConfigLoadedEvent, self.on_config_loaded) \
+                  .listen(ArgParseEvent, self.on_arg_parse) \
                   .listen(ArgParsedEvent, self.on_arg_parsed) \
                   .listen(ScenarioRunEvent, self.on_scenario_run) \
                   .listen(StepPassedEvent, self.on_step_end) \
@@ -63,16 +66,22 @@ class ArtifactedPlugin(Plugin):
                   .listen(ScenarioFailedEvent, self.on_scenario_end) \
                   .listen(ScenarioReportedEvent, self.on_scenario_reported)
 
+    def on_config_loaded(self, event: ConfigLoadedEvent) -> None:
+        self._global_config = event.config
+
     def on_arg_parse(self, event: ArgParseEvent) -> None:
-        event.arg_parser.add_argument("--save-artifacts", action="store_true",
-                                      default=self._save_artifacts, help="<message>")
+        event.arg_parser.add_argument("--save-artifacts",
+                                      action="store_true",
+                                      default=self._save_artifacts,
+                                      help="Save artifacts to file system")
 
     def on_arg_parsed(self, event: ArgParsedEvent) -> None:
         self._save_artifacts = event.args.save_artifacts
 
-        artifacts_path = self._get_artifacts_path()
-        if artifacts_path.exists():
-            shutil.rmtree(artifacts_path, ignore_errors=True)
+        if self._save_artifacts:
+            artifacts_path = self._get_artifacts_dir()
+            if artifacts_path.exists():
+                shutil.rmtree(artifacts_path)
 
     def on_scenario_run(self, event: ScenarioRunEvent) -> None:
         self._scenario_artifacts.clear()
@@ -89,12 +98,29 @@ class ArtifactedPlugin(Plugin):
             artifact = self._scenario_artifacts.popleft()
             event.scenario_result.attach(artifact)
 
-    def _get_artifacts_path(self) -> Path:
-        return Path.cwd() / self._artifacts_dir
+    async def on_scenario_reported(self, event: ScenarioReportedEvent) -> None:
+        aggregated_result = event.aggregated_result
+        for scenario_result in aggregated_result.scenario_results:
+            scenario_artifacts_dir = self._get_scenario_artifacts_dir(scenario_result)
+            self._ensure_exists(scenario_artifacts_dir)
 
-    def _get_scenario_path(self, scenario_result: ScenarioResult) -> Path:
+            for step_result in scenario_result.step_results:
+                for artifact in step_result.artifacts:
+                    self._save_artifact(artifact, scenario_artifacts_dir)
+
+            for artifact in scenario_result.artifacts:
+                self._save_artifact(artifact, scenario_artifacts_dir)
+
+    def _get_project_dir(self) -> Path:
+        assert self._global_config is not None
+        return self._global_config.project_dir
+
+    def _get_artifacts_dir(self) -> Path:
+        return self._get_project_dir() / ".vedro" / "artifacts/"
+
+    def _get_scenario_artifacts_dir(self, scenario_result: ScenarioResult) -> Path:
         scenario = scenario_result.scenario
-        scenario_path = self._get_artifacts_path()
+        scenario_path = self._get_artifacts_dir()
         scenario_path /= scenario.rel_path.with_suffix('')
 
         template_index = str(scenario.template_index or 0)
@@ -103,9 +129,10 @@ class ArtifactedPlugin(Plugin):
 
         return scenario_path
 
-    def _save_artifact(self, artifact: Artifact, scenario_path: Path) -> None:
-        scenario_path.mkdir(parents=True, exist_ok=True)
+    def _ensure_exists(self, path: Path) -> None:
+        path.mkdir(parents=True, exist_ok=True)
 
+    def _save_artifact(self, artifact: Artifact, scenario_path: Path) -> None:
         if isinstance(artifact, MemoryArtifact):
             artifact_path = scenario_path / artifact.name
             artifact_path.write_bytes(artifact.data)
@@ -113,20 +140,14 @@ class ArtifactedPlugin(Plugin):
             artifact_path = scenario_path / artifact.name
             shutil.copy2(artifact.path, artifact_path)
         else:
-            raise Exception("Unknown artifact type")
-
-    async def on_scenario_reported(self, event: ScenarioReportedEvent) -> None:
-        aggregated_result = event.aggregated_result
-        for scenario_result in aggregated_result.scenario_results:
-            for step_result in scenario_result.step_results:
-                for artifact in step_result.artifacts:
-                    self._save_artifact(artifact, self._get_scenario_path(scenario_result))
-            for artifact in scenario_result.artifacts:
-                self._save_artifact(artifact, self._get_scenario_path(scenario_result))
+            artifact_type = type(artifact).__name__
+            rel_path = scenario_path.relative_to(self._get_project_dir())
+            raise TypeError(f"Can't save artifact to '{rel_path}:' unknown type '{artifact_type}'")
 
 
 class Artifacted(PluginConfig):
     plugin = ArtifactedPlugin
     description = "Manages artifacts for step and scenario results"
 
+    # Save artifacts to file system
     save_artifacts: bool = False
