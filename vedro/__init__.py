048a66731d69ba6a647fbf82cd68bf3ed156ae32
Nikita Tsvetkov <tsv1@fastmail.com>
2024-06-07 21:03:44 +0400

    update impl

--- vedro/__init__.py
+++ vedro/__init__.py
@@ -22,9 +22,10 @@ from .plugins.artifacted import (
 from .plugins.deferrer import defer
 from .plugins.skipper import only, skip, skip_if
 from .plugins.temp_keeper import create_tmp_dir, create_tmp_file
+from .plugins.ensurer import ensure
 
 __version__ = version
-__all__ = ("Scenario", "Interface", "run", "only", "skip", "skip_if", "params",
+__all__ = ("Scenario", "Interface", "run", "only", "skip", "skip_if", "params", "ensure",
            "context", "defer", "Config", "catched", "create_tmp_dir", "create_tmp_file",
            "attach_artifact", "attach_scenario_artifact", "attach_step_artifact",
            "MemoryArtifact", "FileArtifact", "Artifact",)
--- vedro/plugins/ensurer/_ensure.py
+++ vedro/plugins/ensurer/_ensure.py
@@ -1,7 +1,7 @@
 import asyncio
 import time
 from functools import wraps
-from typing import Any, Callable, Coroutine, Optional, Tuple, Type, TypeVar, Union, overload
+from typing import Any, Callable, Coroutine, Optional, Tuple, Type, TypeVar, Union, cast, overload
 
 __all__ = ("ensure",)
 
@@ -33,34 +33,40 @@ class ensure:
     def __call__(self, fn: AF) -> AF:
         ...
 
-    def __call__(self, fn:  Any) -> Any:
+    def __call__(self, fn: Any) -> Any:
         if asyncio.iscoroutinefunction(fn):
-            @wraps(fn)
-            async def async_wrapper(*args: Any, **kwargs: Any) -> Any:
-                exception = None
-                for attempt in range(self.attempts):
-                    try:
-                        return await fn(*args, **kwargs)
-                    except self._swallow as e:
-                        exception = e
-                        delay = self.delay() if callable(self.delay) else self.delay
-                        await asyncio.sleep(delay)
-                if exception:
-                    raise exception
-
-            return async_wrapper
+            return self._async_wrapper(fn)
         else:
-            @wraps(fn)
-            def sync_wrapper(*args: Any, **kwargs: Any) -> Any:
-                exception = None
-                for attempt in range(self.attempts):
-                    try:
-                        return fn(*args, **kwargs)
-                    except self._swallow as e:
-                        exception = e
-                        delay = self.delay() if callable(self.delay) else self.delay
-                        time.sleep(delay)
-                if exception:
-                    raise exception
+            return self._sync_wrapper(fn)
 
-            return sync_wrapper
+    def _sync_wrapper(self, fn: F) -> F:
+        @wraps(fn)
+        def sync_wrapper(*args: Any, **kwargs: Any) -> Any:
+            exception = None
+            for _ in range(self.attempts):
+                try:
+                    return fn(*args, **kwargs)
+                except self._swallow as e:
+                    exception = e
+                    delay = self.delay() if callable(self.delay) else self.delay
+                    time.sleep(delay)
+            if exception:
+                raise exception
+
+        return cast(F, sync_wrapper)
+
+    def _async_wrapper(self, fn: AF) -> AF:
+        @wraps(fn)
+        async def async_wrapper(*args: Any, **kwargs: Any) -> Any:
+            exception = None
+            for _ in range(self.attempts):
+                try:
+                    return await fn(*args, **kwargs)
+                except self._swallow as e:
+                    exception = e
+                    delay = self.delay() if callable(self.delay) else self.delay
+                    await asyncio.sleep(delay)
+            if exception:
+                raise exception
+
+        return cast(AF, async_wrapper)
