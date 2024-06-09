25c0cf9f31e1f7dc6ae5788bd5023d4e77371727
Nikita Tsvetkov <tsv1@fastmail.com>
2024-06-09 19:22:32 +0400

    add docstrings

--- vedro/plugins/ensurer/_ensure.py
+++ vedro/plugins/ensurer/_ensure.py
@@ -17,14 +17,30 @@ DelayType = Union[DelayValueType, DelayCallableType]
 ExceptionType = Type[BaseException]
 SwallowExceptionType = Union[Tuple[ExceptionType, ...], ExceptionType]
 
-LoggerType = Callable[[Any, AttemptType, Union[BaseException, None]], Any]
+LoggerType = Callable[[Callable[..., Any], AttemptType, Union[BaseException, None]], Any]
 
 
 class Ensure:
+    """
+    Provides functionality to ensure a function succeeds within a specified number of attempts.
+
+    This class retries a given function or coroutine function a specified number of times,
+    optionally with a delay between attempts, and can log each attempt.
+    """
+
     def __init__(self, *, attempts: AttemptType = 3,
                  delay: DelayType = 0.0,
                  swallow: SwallowExceptionType = BaseException,
                  logger: Optional[LoggerType] = None) -> None:
+        """
+        Initialize the Ensure instance with retry configurations.
+
+        :param attempts: The number of attempts to try executing the function. Default is 3.
+        :param delay: The delay between attempts. Can be a fixed value or a callable
+            returning a value.
+        :param swallow: The exception(s) to be caught and retried. Default is BaseException.
+        :param logger: An optional logging callable to log each attempt. Default is None.
+        """
         self._attempts = attempts
         self._delay = delay
         self._swallow = swallow
@@ -32,19 +48,43 @@ class Ensure:
 
     @overload
     def __call__(self, fn: F) -> F:
+        """
+        Apply the Ensure decorator to a synchronous function.
+
+        :param fn: The synchronous function to be decorated.
+        :return: The decorated function.
+        """
         ...
 
     @overload
     def __call__(self, fn: AF) -> AF:
+        """
+        Apply the Ensure decorator to an asynchronous function.
+
+        :param fn: The asynchronous function to be decorated.
+        :return: The decorated coroutine function.
+        """
         ...
 
     def __call__(self, fn: Any) -> Any:
+        """
+        Determine whether the function is async or async and apply the appropriate wrapper.
+
+        :param fn: The function to be decorated.
+        :return: The decorated function or coroutine function.
+        """
         if asyncio.iscoroutinefunction(fn):
             return self._async_wrapper(fn)
         else:
             return self._sync_wrapper(fn)
 
     def _sync_wrapper(self, fn: F) -> F:
+        """
+        Wrap a synchronous function with retry logic.
+
+        :param fn: The synchronous function to be wrapped.
+        :return: The wrapped function with retry logic.
+        """
         @wraps(fn)
         def sync_wrapper(*args: Any, **kwargs: Any) -> Any:
             exception = None
@@ -66,6 +106,12 @@ class Ensure:
         return cast(F, sync_wrapper)
 
     def _async_wrapper(self, fn: AF) -> AF:
+        """
+        Wrap an asynchronous function with retry logic.
+
+        :param fn: The asynchronous function to be wrapped.
+        :return: The wrapped coroutine function with retry logic.
+        """
         @wraps(fn)
         async def async_wrapper(*args: Any, **kwargs: Any) -> Any:
             exception = None
@@ -87,5 +133,10 @@ class Ensure:
         return cast(AF, async_wrapper)
 
     def __repr__(self) -> str:
+        """
+        Return a string representation of the Ensure instance.
+
+        :return: A string representation of the Ensure instance with its configurations.
+        """
         return (f"{self.__class__.__name__}"
                 f"(attempts={self._attempts}, delay={self._delay!r}, swallow={self._swallow!r})")
