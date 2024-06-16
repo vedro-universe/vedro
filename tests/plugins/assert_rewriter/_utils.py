ad8391c62078f78aff63d6119d710806f60cc9ab
Nikita Tsvetkov <tsv1@fastmail.com>
2024-06-16 14:28:01 +0400

    add tests

--- tests/plugins/assert_rewriter/_utils.py
+++ tests/plugins/assert_rewriter/_utils.py
@@ -1,9 +1,15 @@
 import os
+from argparse import ArgumentParser, Namespace
 from pathlib import Path
 
 import pytest
 
-__all__ = ("tmp_scn_dir",)
+from vedro.core import Config, ConfigType, Dispatcher, Factory, ModuleFileLoader, ModuleLoader
+from vedro.events import ArgParsedEvent, ArgParseEvent, ConfigLoadedEvent
+from vedro.plugins.assert_rewriter import AssertRewriter, AssertRewriterPlugin
+
+__all__ = ("tmp_scn_dir", "dispatcher", "assert_rewriter_plugin", "make_config",
+           "fire_config_loaded_event", "fire_arg_parsed_event",)
 
 
 @pytest.fixture()
@@ -17,3 +23,39 @@ def tmp_scn_dir(tmp_path: Path) -> Path:
         yield scn_dir.relative_to(tmp_path)
     finally:
         os.chdir(cwd)
+
+
+@pytest.fixture()
+def dispatcher() -> Dispatcher:
+    return Dispatcher()
+
+
+@pytest.fixture()
+def assert_rewriter_plugin(dispatcher: Dispatcher) -> AssertRewriterPlugin:
+    plugin = AssertRewriterPlugin(AssertRewriter)
+    plugin.subscribe(dispatcher)
+    return plugin
+
+
+def make_config() -> ConfigType:
+    class TestConfig(Config):
+        class Registry(Config.Registry):
+            ModuleLoader = Factory[ModuleLoader](ModuleFileLoader)
+
+    return TestConfig
+
+
+async def fire_config_loaded_event(dispatcher: Dispatcher, config: ConfigType) -> None:
+    config_loaded_event = ConfigLoadedEvent(Path(), config)
+    await dispatcher.fire(config_loaded_event)
+
+
+async def fire_arg_parsed_event(
+        dispatcher: Dispatcher, *,
+        legacy_assertions: bool = AssertRewriter.legacy_assertions
+        ) -> None:
+    arg_parse_event = ArgParseEvent(ArgumentParser())
+    await dispatcher.fire(arg_parse_event)
+
+    arg_parsed_event = ArgParsedEvent(Namespace(legacy_assertions=legacy_assertions))
+    await dispatcher.fire(arg_parsed_event)
--- vedro/plugins/assert_rewriter/_assert_rewriter_plugin.py
+++ vedro/plugins/assert_rewriter/_assert_rewriter_plugin.py
@@ -12,6 +12,7 @@ __all__ = ("AssertRewriter", "AssertRewriterPlugin",)
 class AssertRewriterPlugin(Plugin):
     def __init__(self, config: Type["AssertRewriter"]):
         super().__init__(config)
+        self._legacy_assertions = config.legacy_assertions
 
     def subscribe(self, dispatcher: Dispatcher) -> None:
         dispatcher.listen(ConfigLoadedEvent, self.on_config_loaded) \
@@ -22,15 +23,21 @@ class AssertRewriterPlugin(Plugin):
         self._global_config: ConfigType = event.config
 
     def on_arg_parse(self, event: ArgParseEvent) -> None:
-        event.arg_parser.add_argument("--exp-pretty-diff", action="store_true", default=False,
-                                      help="")
+        help_message = "Use legacy assertion rewriter for backwards compatibility"
+        event.arg_parser.add_argument("--legacy-assertions", action="store_true",
+                                      default=self._legacy_assertions, help=help_message)
 
     def on_arg_parsed(self, event: ArgParsedEvent) -> None:
-        exp_pretty_diff = event.args.exp_pretty_diff
-        module_loader = AssertRewriterLoader if exp_pretty_diff else LegacyAssertRewriterLoader
-        self._global_config.Registry.ModuleLoader.register(module_loader, self)
+        self._legacy_assertions = event.args.legacy_assertions
+        self._global_config.Registry.ModuleLoader.register(
+            LegacyAssertRewriterLoader if self._legacy_assertions else AssertRewriterLoader,
+            self
+        )
 
 
 class AssertRewriter(PluginConfig):
     plugin = AssertRewriterPlugin
     description = "Rewrites assert statements to provide better error messages"
+
+    # Use legacy assertion rewriter for backwards compatibility
+    legacy_assertions: bool = False
