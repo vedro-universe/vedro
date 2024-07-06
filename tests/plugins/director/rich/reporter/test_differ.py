b7575e6584adffc272a7506230c5382184025dd3
Nikita Tsvetkov <tsv1@fastmail.com>
2024-07-06 15:55:33 +0400

    add tests

--- tests/plugins/director/rich/reporter/test_differ.py
+++ tests/plugins/director/rich/reporter/test_differ.py
@@ -78,6 +78,31 @@ def test_compare_fancy_replace(*, differ: AdvancedDiffer):
         ]
 
 
+def test_compare_fancy_replace_multiple_groups(differ: AdvancedDiffer):
+    with given:
+        a = ["line1", "line2_changed1", "line3", "line4", "line5_changed1", "line6"]
+        b = ["line1", "line2_changed2", "line3", "line4", "line5_changed2", "line6"]
+
+    with when:
+        result = list(differ.compare(a, b))
+
+    with then:
+        assert result == [
+            "  line1",
+            "- line2_changed1",
+            "?              ^\n",
+            "+ line2_changed2",
+            "?              ^\n",
+            "  line3",
+            "  line4",
+            "- line5_changed1",
+            "?              ^\n",
+            "+ line5_changed2",
+            "?              ^\n",
+            "  line6",
+        ]
+
+
 @pytest.mark.parametrize("context_lines", [0, 1, 2])
 def test_compare_unified_equal(context_lines: int, *, differ: AdvancedDiffer):
     with given:
@@ -174,6 +199,60 @@ def test_compare_unified_fancy_replace(context_lines: int, expected: List[str], 
     with given:
         a = ["line1", "line2_changed1", "line3"]
         b = ["line1", "line2_changed2", "line3"]
+
+    with when:
+        result = list(differ.compare_unified(a, b, context_lines))
+
+    with then:
+        assert result == expected
+
+
+@pytest.mark.parametrize(("context_lines", "expected"), [
+    (0, [
+        "- line2_changed1",
+        "?              ^\n",
+        "+ line2_changed2",
+        "?              ^\n",
+        "...",
+        "- line5_changed1",
+        "?              ^\n",
+        "+ line5_changed2",
+        "?              ^\n"
+    ]),
+    (1, [
+        "  line1",
+        "- line2_changed1",
+        "?              ^\n",
+        "+ line2_changed2",
+        "?              ^\n",
+        "  line3",
+        "  line4",
+        "- line5_changed1",
+        "?              ^\n",
+        "+ line5_changed2",
+        "?              ^\n",
+        "  line6"
+    ]),
+    (2, [
+        "  line1",
+        "- line2_changed1",
+        "?              ^\n",
+        "+ line2_changed2",
+        "?              ^\n",
+        "  line3",
+        "  line4",
+        "- line5_changed1",
+        "?              ^\n",
+        "+ line5_changed2",
+        "?              ^\n",
+        "  line6"
+    ]),
+])
+def test_compare_unified_fancy_replace_multiple_groups(context_lines: int, expected: List[str], *,
+                                                       differ: AdvancedDiffer):
+    with given:
+        a = ["line1", "line2_changed1", "line3", "line4", "line5_changed1", "line6"]
+        b = ["line1", "line2_changed2", "line3", "line4", "line5_changed2", "line6"]
 
     with when:
         result = list(differ.compare_unified(a, b, context_lines))
