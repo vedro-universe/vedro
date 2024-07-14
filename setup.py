05328824782b28b3b9dbec688517ec6d81508260
Nikita Tsvetkov <tsv1@fastmail.com>
2024-07-14 22:07:16 +0400

    bump version → 1.12.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def find_dev_required():
 
 setup(
     name="vedro",
-    version="1.11.2",
+    version="1.12.0",
     description="Pragmatic Testing Framework",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
