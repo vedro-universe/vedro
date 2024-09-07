1f768c919bc2089c31837fa4f49f2166773382ea
Nikita Tsvetkov <tsv1@fastmail.com>
2024-09-07 14:09:38 +0400

    add docstrings

--- vedro/plugins/orderer/reversed_orderer.py
+++ vedro/plugins/orderer/reversed_orderer.py
@@ -7,5 +7,18 @@ __all__ = ("ReversedOrderer",)
 
 
 class ReversedOrderer(StableScenarioOrderer):
+    """
+    A scenario orderer that reverses the order of scenarios.
+
+    The `ReversedOrderer` reverses the order of the scenarios provided
+    by the `StableScenarioOrderer`.
+    """
+
     async def sort(self, scenarios: List[VirtualScenario]) -> List[VirtualScenario]:
+        """
+        Sort the scenarios in reversed order.
+
+        :param scenarios: The list of `VirtualScenario` instances to be sorted.
+        :return: A new list of scenarios in reversed order.
+        """
         return list(reversed(await super().sort(scenarios)))
