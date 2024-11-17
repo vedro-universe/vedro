9f2b2b7ec6770f00d5be89027ae76b77c3409700
Nikitа Tsvetkоv <tsv1@fastmail.com>
2024-11-17 11:45:07 +0400

    Add global deferral support to DeferrerPlugin
    Fixes #99
    
    Add support for global deferral of functions in `DeferrerPlugin`.

--- tests/plugins/deferrer/test_deferrer_plugin.py
+++ tests/plugins/deferrer/test_deferrer_plugin.py
@@ -4,9 +4,9 @@ from unittest.mock import AsyncMock, Mock, call
 import pytest
 from baby_steps import given, then, when
 
-from vedro import defer
+from vedro import defer, defer_global
 from vedro.core import Dispatcher, ScenarioResult
-from vedro.events import ScenarioFailedEvent, ScenarioPassedEvent, ScenarioRunEvent
+from vedro.events import ScenarioFailedEvent, ScenarioPassedEvent, ScenarioRunEvent, CleanupEvent
 from vedro.plugins.deferrer import Deferrer, DeferrerPlugin
 
 from ._utils import deferrer, dispatcher, make_vscenario, queue
@@ -118,3 +118,73 @@ async def test_(event_class, *, dispatcher: Dispatcher):
             call.deferred1(*args1, **kwargs1),
         ]
         deferred1.assert_awaited_once()
+
+
+@pytest.mark.usefixtures(deferrer.__name__)
+async def test_cleanup_event(*, dispatcher: Dispatcher, queue: deque):
+    with given:
+        queue.append((Mock(), (), {}))
+
+        event = CleanupEvent(Mock())
+
+    with when:
+        await dispatcher.fire(event)
+
+    with then:
+        assert len(queue) == 0
+
+
+@pytest.mark.usefixtures(deferrer.__name__)
+async def test_global_deferral(*, dispatcher: Dispatcher, queue: deque):
+    with given:
+        manager = Mock()
+        deferred1 = Mock()
+        manager.attach_mock(deferred1, "deferred1")
+        deferred2 = Mock()
+        manager.attach_mock(deferred2, "deferred2")
+
+        args1, kwargs1 = ("arg1", "arg2"), {"key1": "val1"}
+        defer_global(deferred1, *args1, **kwargs1)
+        args2, kwargs2 = ("arg3", "arg4"), {"key2": "val2"}
+        defer_global(deferred2, *args2, **kwargs2)
+
+        event = CleanupEvent(Mock())
+
+    with when:
+        await dispatcher.fire(event)
+
+    with then:
+        assert manager.mock_calls == [
+            call.deferred2(*args2, **kwargs2),
+            call.deferred1(*args1, **kwargs1),
+        ]
+        assert len(queue) == 0
+
+
+@pytest.mark.usefixtures(deferrer.__name__)
+async def test_global_deferral_async(*, dispatcher: Dispatcher, queue: deque):
+    with given:
+        manager = Mock()
+        deferred1 = AsyncMock()
+        manager.attach_mock(deferred1, "deferred1")
+        deferred2 = AsyncMock()
+        manager.attach_mock(deferred2, "deferred2")
+
+        args1, kwargs1 = ("arg1", "arg2"), {"key1": "val1"}
+        defer_global(deferred1, *args1, **kwargs1)
+        args2, kwargs2 = ("arg3", "arg4"), {"key2": "val2"}
+        defer_global(deferred2, *args2, **kwargs2)
+
+        event = CleanupEvent(Mock())
+
+    with when:
+        await dispatcher.fire(event)
+
+    with then:
+        assert manager.mock_calls == [
+            call.deferred2(*args2, **kwargs2),
+            call.deferred1(*args1, **kwargs1),
+        ]
+        deferred1.assert_awaited_once()
+        deferred2.assert_awaited_once()
+        assert len(queue) == 0
--- vedro/plugins/deferrer/_deferrer.py
+++ vedro/plugins/deferrer/_deferrer.py
@@ -3,14 +3,15 @@ from collections import deque
 from typing import Any, Callable, Deque, Dict, Tuple, Type, Union, final
 
 from vedro.core import Dispatcher, Plugin, PluginConfig
-from vedro.events import ScenarioFailedEvent, ScenarioPassedEvent, ScenarioRunEvent
+from vedro.events import ScenarioFailedEvent, ScenarioPassedEvent, ScenarioRunEvent, CleanupEvent
 
-__all__ = ("Deferrer", "DeferrerPlugin", "defer", "Deferrable",)
+__all__ = ("Deferrer", "DeferrerPlugin", "defer", "defer_global", "Deferrable",)
 
 
 Deferrable = Tuple[Callable[..., Any], Tuple[Any, ...], Dict[str, Any]]
 
 _queue: Deque[Deferrable] = deque()
+_global_queue: Deque[Deferrable] = deque()
 
 
 def defer(fn: Callable[..., Any], *args: Any, **kwargs: Any) -> None:
@@ -30,25 +31,43 @@ def defer(fn: Callable[..., Any], *args: Any, **kwargs: Any) -> None:
     _queue.append((fn, args, kwargs))
 
 
+def defer_global(fn: Callable[..., Any], *args: Any, **kwargs: Any) -> None:
+    """
+    Defer the execution of a function until the end of the entire test session.
+
+    The function will be added to a global queue and executed after all scenarios have completed.
+
+    :param fn: The function to be deferred.
+    :param args: Positional arguments to be passed to the function.
+    :param kwargs: Keyword arguments to be passed to the function.
+    """
+    _global_queue.append((fn, args, kwargs))
+
+
 @final
 class DeferrerPlugin(Plugin):
     """
-    A plugin that defers the execution of functions until the end of a scenario.
+    A plugin that defers the execution of functions until the end of a scenario or the entire test session.
 
     This plugin listens to scenario events and ensures that deferred functions are executed
     after the scenario has either passed or failed. The deferred functions can be regular
-    functions or coroutines.
+    functions or coroutines. It also supports global deferral of functions to be executed
+    at the end of the entire test session.
     """
 
-    def __init__(self, config: Type["Deferrer"], *, queue: Deque[Deferrable] = _queue) -> None:
+    def __init__(self, config: Type["Deferrer"], *,
+                 queue: Deque[Deferrable] = _queue,
+                 global_queue: Deque[Deferrable] = _global_queue) -> None:
         """
         Initialize the DeferrerPlugin with the provided configuration.
 
         :param config: The Deferrer configuration class.
         :param queue: The queue holding deferred functions.
+        :param global_queue: The global queue holding deferred functions for the entire test session.
         """
         super().__init__(config)
         self._queue = queue
+        self._global_queue = global_queue
 
     def subscribe(self, dispatcher: Dispatcher) -> None:
         """
@@ -58,7 +77,8 @@ class DeferrerPlugin(Plugin):
         """
         dispatcher.listen(ScenarioRunEvent, self.on_scenario_run) \
                   .listen(ScenarioPassedEvent, self.on_scenario_end) \
-                  .listen(ScenarioFailedEvent, self.on_scenario_end)
+                  .listen(ScenarioFailedEvent, self.on_scenario_end) \
+                  .listen(CleanupEvent, self.on_cleanup)
 
     def on_scenario_run(self, event: ScenarioRunEvent) -> None:
         """
@@ -85,14 +105,30 @@ class DeferrerPlugin(Plugin):
             else:
                 fn(*args, **kwargs)
 
+    async def on_cleanup(self, event: CleanupEvent) -> None:
+        """
+        Handle the event when the test session ends, executing all globally deferred functions.
+
+        Globally deferred functions are executed in reverse order (LIFO). If a deferred function
+        is a coroutine, it will be awaited. Otherwise, it will be called as a regular function.
+
+        :param event: The CleanupEvent instance.
+        """
+        while len(self._global_queue) > 0:
+            fn, args, kwargs = self._global_queue.pop()
+            if iscoroutinefunction(fn):
+                await fn(*args, **kwargs)
+            else:
+                fn(*args, **kwargs)
+
 
 class Deferrer(PluginConfig):
     """
     Configuration class for the DeferrerPlugin.
 
     This class defines the default behavior for deferring functions during scenario execution.
-    It ensures that functions are executed at the end of each scenario.
+    It ensures that functions are executed at the end of each scenario and supports global deferral.
     """
 
     plugin = DeferrerPlugin
-    description = "Executes deferred functions at the end of each scenario"
+    description = "Executes deferred functions at the end of each scenario and globally at the end of the test session"
