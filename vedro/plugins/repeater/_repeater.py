4160ee1182d6aec36f9ed20b87f85147335e224c
Nikita Tsvetkov <tsv1@fastmail.com>
2024-12-20 21:00:44 +0400

    unify scheduler type checking

--- vedro/plugins/repeater/_repeater.py
+++ vedro/plugins/repeater/_repeater.py
@@ -193,7 +193,7 @@ class RepeaterPlugin(Plugin):
         """
         if not self._is_repeating_enabled():
             return
-        assert isinstance(self._scheduler, RepeaterScenarioScheduler)  # for type checking
+        assert isinstance(self._scheduler, ScenarioScheduler)  # for type checking
 
         scenario = event.scenario_result.scenario
         if scenario.unique_id != self._repeat_scenario_id:
