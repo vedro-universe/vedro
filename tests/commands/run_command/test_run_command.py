8b1eea192368bc8c4b8613ea5b5373596769ae3a
Nikita Tsvetkov <tsv1@fastmail.com>
2025-01-05 16:50:56 +0400

    add tests for various invalid scenario directory cases

--- tests/commands/run_command/test_run_command.py
+++ tests/commands/run_command/test_run_command.py
@@ -24,9 +24,26 @@ class CustomConfig(Config):
             pass
 
 
-async def test_run_command_without_scenarios(arg_parser: ArgumentParser):
+async def test_run_command_with_invalid_type_scenario_dir(arg_parser: ArgumentParser):
     with given:
-        command = RunCommand(CustomConfig, arg_parser)
+        class CustomScenarioDir(CustomConfig):
+            default_scenarios_dir = None
+        command = RunCommand(CustomScenarioDir, arg_parser)
+
+    with when, raises(BaseException) as exc:
+        await command.run()
+
+    with then:
+        assert exc.type is TypeError
+        assert "default_scenarios_dir" in str(exc.value)
+        assert "to be a Path" in str(exc.value)
+
+
+async def test_run_command_with_nonexistent_scenario_dir(arg_parser: ArgumentParser):
+    with given:
+        class CustomScenarioDir(CustomConfig):
+            default_scenarios_dir = "nonexisting/"
+        command = RunCommand(CustomScenarioDir, arg_parser)
 
     with when, raises(BaseException) as exc:
         await command.run()
@@ -35,6 +52,52 @@ async def test_run_command_without_scenarios(arg_parser: ArgumentParser):
         assert exc.type is FileNotFoundError
         assert "default_scenarios_dir" in str(exc.value)
         assert "does not exist" in str(exc.value)
+
+
+async def test_run_command_with_non_directory_scenario_dir(tmp_dir: Path, arg_parser: ArgumentParser):
+    with given:
+        existing_file = tmp_dir / "scenario.py"
+        existing_file.touch()
+
+        class CustomScenarioDir(CustomConfig):
+            default_scenarios_dir = existing_file
+        command = RunCommand(CustomScenarioDir, arg_parser)
+
+    with when, raises(BaseException) as exc:
+        await command.run()
+
+    with then:
+        assert exc.type is NotADirectoryError
+        assert "default_scenarios_dir" in str(exc.value)
+        assert "is not a directory" in str(exc.value)
+
+
+@pytest.mark.usefixtures(tmp_dir.__name__)
+async def test_run_command_with_scenario_dir_outside_project_dir(arg_parser: ArgumentParser):
+    with given:
+        class CustomScenarioDir(CustomConfig):
+            default_scenarios_dir = "/tmp"
+        command = RunCommand(CustomScenarioDir, arg_parser)
+
+    with when, raises(BaseException) as exc:
+        await command.run()
+
+    with then:
+        assert exc.type is ValueError
+        assert "default_scenarios_dir" in str(exc.value)
+        assert "must be inside project directory" in str(exc.value)
+
+
+async def test_run_command_without_scenarios(arg_parser: ArgumentParser):
+    with given:
+        command = RunCommand(CustomConfig, arg_parser)
+
+    with when, raises(BaseException) as exc:
+        await command.run()
+
+    with then:
+        assert exc.type is SystemExit
+        assert str(exc.value) == "1"
 
 
 @pytest.mark.usefixtures(tmp_dir.__name__)
