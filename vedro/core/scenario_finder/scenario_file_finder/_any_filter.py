459ba529ad439195bc53bdd9af866151a6dd0058
Nikita Tsvetkov <tsv1@fastmail.com>
2025-01-06 23:07:10 +0400

    add docstrings

--- vedro/core/scenario_finder/scenario_file_finder/_any_filter.py
+++ vedro/core/scenario_finder/scenario_file_finder/_any_filter.py
@@ -7,10 +7,27 @@ __all__ = ("AnyFilter",)
 
 
 class AnyFilter(FileFilter):
+    """
+    Combines multiple filters using a logical OR operation.
+
+    This filter passes a path if at least one of the provided filters passes the path.
+    """
+
     def __init__(self, filters: List[FileFilter]) -> None:
+        """
+        Initialize the AnyFilter with a list of filters.
+
+        :param filters: A list of FileFilter objects to combine.
+        """
         self._filters = filters
 
     def filter(self, path: Path) -> bool:
+        """
+        Check if the given path passes any of the filters.
+
+        :param path: The file or directory path to evaluate.
+        :return: True if at least one filter passes the path, otherwise False.
+        """
         for filter_ in self._filters:
             if filter_.filter(path):
                 return True
--- vedro/core/scenario_orderer/_scenario_orderer.py
+++ vedro/core/scenario_orderer/_scenario_orderer.py
@@ -7,6 +7,24 @@ __all__ = ("ScenarioOrderer",)
 
 
 class ScenarioOrderer(ABC):
+    """
+    Abstract base class for scenario orderers.
+
+    This class defines the interface for sorting a list of scenarios. Concrete
+    implementations of this class should define specific ordering logic.
+    """
+
     @abstractmethod
     async def sort(self, scenarios: List[VirtualScenario]) -> List[VirtualScenario]:
+        """
+        Sort the given list of scenarios.
+
+        This is an abstract method that must be implemented by subclasses. It should
+        return a sorted list of `VirtualScenario` objects based on a specific ordering
+        logic.
+
+        :param scenarios: A list of `VirtualScenario` instances to be sorted.
+        :return: A new list of `VirtualScenario` instances in the desired order.
+        :raises NotImplementedError: If the method is not implemented in a subclass.
+        """
         pass
