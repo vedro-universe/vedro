ffb6346b3b92c689e207604af994c6110aaae2d7
Nikita Tsvetkov <tsv1@fastmail.com>
2025-02-16 15:24:45 +0400

    add support for Python 3.10+ with ParamSpec and Callable types for enhanced type checking

--- vedro/_context.py
+++ vedro/_context.py
@@ -1,10 +1,21 @@
-from typing import TypeVar
+import sys
 
 __all__ = ("context",)
 
-T = TypeVar("T")
+if sys.version_info >= (3, 10):
+    from typing import Callable, ParamSpec, TypeVar
 
+    P = ParamSpec("P")
+    R = TypeVar("R")
 
-def context(fn: T) -> T:
-    setattr(fn, "__vedro__context__", True)
-    return fn
+    def context(fn: Callable[P, R]) -> Callable[P, R]:
+        setattr(fn, "__vedro__context__", True)
+        return fn
+else:
+    from typing import TypeVar
+
+    T = TypeVar("T")
+
+    def context(fn: T) -> T:
+        setattr(fn, "__vedro__context__", True)
+        return fn
