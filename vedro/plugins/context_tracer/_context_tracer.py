6bc46b5604dd760a1e4eeb673472e6d60f8c8715
Nikita Tsvetkov <tsv1@fastmail.com>
2025-08-24 10:49:10 +0400

    Add typing

--- vedro/plugins/context_tracer/_context_tracer.py
+++ vedro/plugins/context_tracer/_context_tracer.py
@@ -98,3 +98,24 @@ class ContextTracerPlugin(Plugin):
 class ContextTracer(PluginConfig):
     plugin = ContextTracerPlugin
     description = ""
+
+
+# import sys
+
+# if sys.version_info >= (3, 10):
+#     from typing import Callable, ParamSpec, TypeVar
+
+#     P = ParamSpec("P")
+#     R = TypeVar("R")
+
+#     def context(fn: Callable[P, R]) -> Callable[P, R]:
+#         setattr(fn, "__vedro__context__", True)
+#         return fn
+# else:
+#     from typing import TypeVar
+
+#     T = TypeVar("T")
+
+#     def context(fn: T) -> T:
+#         setattr(fn, "__vedro__context__", True)
+#         return fn
