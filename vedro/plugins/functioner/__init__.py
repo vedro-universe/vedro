a4cdc28576947a7564f806cab36bb7507b8cc698
Nikita Tsvetkov <tsv1@fastmail.com>
2025-09-07 12:34:42 +0400

    Rename _func_based_scenario_provider.py to _scenario_provider.py and update imports accordingly

--- vedro/plugins/functioner/__init__.py
+++ vedro/plugins/functioner/__init__.py
@@ -1,6 +1,6 @@
-from ._func_based_scenario_provider import FuncBasedScenarioProvider
 from ._functioner import Functioner, FunctionerPlugin
 from ._scenario_decorator import scenario
+from ._scenario_provider import FuncBasedScenarioProvider
 from ._scenario_steps import given, then, when
 
 __all__ = ("scenario", "given", "when", "then",
